649c85072648cf14c33ecc97889a1be2a57c52ea
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-05-24 06:40:53 -0400

    Create javascript code to show and hide the banner bar in the header section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
+function headerBanner() {
+  const scrollYPosition = window.scrollY;
+  const banner = document.querySelector('.banner');
+  if(scrollYPosition < 100) {
+    banner.classList.add('show-banner');
+  } else {
+    banner.classList.remove('show-banner');
+  }
+}
+document.addEventListener('scroll', headerBanner);
+
 const getYear = new Date();
 document.querySelector('#copyright-year').innerHTML = getYear.getFullYear();
 
