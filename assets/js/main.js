64aa2d74f24899783699742a008025bc83191c88
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-05-24 05:04:28 -0400

    Create javascript code to show and hide the mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,18 @@
 const getYear = new Date();
 document.querySelector('#copyright-year').innerHTML = getYear.getFullYear();
+
+const menuButton = document.querySelector('#open-mobile-menu');
+const mobileMenu = document.querySelector('#mobile-menu');
+const navbar = document.querySelector('#navbar');
+menuButton.addEventListener('click', function() {
+  menuButton.classList.toggle('active');
+  if(mobileMenu.classList.contains('show')) {
+    mobileMenu.classList.remove('show');
+    mobileMenu.classList.add('hidden');
+    navbar.classList.remove('hidden');
+  } else {
+    navbar.classList.add('hidden');
+    mobileMenu.classList.remove('hidden');
+    mobileMenu.classList.add('show');
+  }
+});
